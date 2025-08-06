7e91e2c65a46b8d909ecaa37c8222ab759689cfa
xudandan <xudandan@lattebank.com>
2025-08-06 19:59:39 +0800

    feat: 加入点光源、环境光和方便调试的坐标系

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,82 @@
 import { ReactElement, useEffect } from 'react';
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
+import { 
+  Scene, 
+  PerspectiveCamera, 
+  WebGLRenderer, 
+  BoxGeometry, 
+  MeshLambertMaterial, 
+  Mesh, 
+  AmbientLight,
+  PointLight,
+  AxesHelper,
+  PointLightHelper
+} from 'three';
+// @ts-ignore
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export default function HomePage() {
   const scene = new Scene();
+  const width = 800;
+  const height = 500;
 
   useEffect(() => {
-    const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const camera = new PerspectiveCamera(45, width / height, 1, 1000);
+    camera.position.set(200, 100, 90);
+    camera.lookAt(0,0,0);  
+
     const renderer = new WebGLRenderer();
-    renderer.setSize( 300, 300 );
+    renderer.setSize(width, height);
+
+    const geometry = new BoxGeometry( 20, 20, 20 );
+    const material = new MeshLambertMaterial( { 
+      color: '#0000ff',
+      opacity: 0.5, // 设置透明度
+      transparent: true, // 开启透明度
+    } );
+    const mesh = new Mesh( geometry, material );
+    scene.add( mesh );
+    // mesh.position.set(0, 100, 0);
+
+    // 添加一个点光源
+    const pointLight = new PointLight('#fff', 0.4);
+    pointLight.position.set(400, 200, 300);
+    scene.add(pointLight)
+
+    // 添加一个环境光
+    const ambient = new AmbientLight('#fff', 1.0);
+    scene.add(ambient);
+
+    const axesHelper = new AxesHelper(50);
+    scene.add( axesHelper );
+
+    // 可视化点光源
+    const pointLightHelper = new PointLightHelper(pointLight, 10);
+    scene.add(pointLightHelper);
+
+    renderer.render( scene, camera );
+
+    
+
+    // function animate() {
+    //   requestAnimationFrame( animate );
+
+    //   mesh.rotation.x += 0.01;
+    //   mesh.rotation.y += 0.01;
+    //   mesh.rotation.z += 0.01;
+
+    //   renderer.render( scene, camera );
+    // }
+
+    // animate();
+    // 设置相机控件轨道控制器OrbitControls
+    const controls = new OrbitControls(camera, renderer.domElement);
+    // 如果OrbitControls改变了相机参数，重新调用渲染器渲染三维场景
+    controls.addEventListener('change', function () {
+        renderer.render(scene, camera); //执行渲染操作
+    });//监听鼠标、键盘事件
+
     const dm = document.querySelector('#rrot');
     dm && dm.appendChild(renderer.domElement);
-
-    const geometry = new BoxGeometry( 1, 1, 1 );
-    const material = new MeshBasicMaterial( { color: '0x00ff00' } );
-    const cube = new Mesh( geometry, material );
-    scene.add( cube );
-
-    camera.position.z = 2;
-
-    function animate() {
-      requestAnimationFrame( animate );
-
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
-      renderer.render( scene, camera );
-    }
-
-    animate();
   }, [])
 
   return (
